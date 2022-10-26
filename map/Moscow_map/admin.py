bb94209ac3e31b65a42a0792690b21297c8d35fc
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2022-10-26 13:19:53 +0300

    Add MapModels model

--- map/Moscow_map/admin.py
+++ map/Moscow_map/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import MapModels
 
+admin.site.register(MapModels)
 # Register your models here.
--- map/Moscow_map/models.py
+++ map/Moscow_map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class MapModels(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.CharField(max_length=500)
+    text = models.TextField()
+    coordinates_lng = models.CharField(max_length=200)
+    coordinates_lat = models.CharField(max_length=200)
 # Create your models here.
