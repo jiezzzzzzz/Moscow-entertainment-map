299ca504313532e0b5731d3a54f678a13c071683
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-03-03 14:34:49 +0300

    added the ability to add images from Places

--- map/places/admin.py
+++ map/places/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Places, Image
 
-admin.site.register(Places)
 admin.site.register(Image)
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Places)
+class PlacesAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
