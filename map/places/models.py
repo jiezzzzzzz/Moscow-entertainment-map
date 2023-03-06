f345fa9428e56d727f55e295b2ff359410e4c6a1
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-03-06 19:19:49 +0300

    added new migrations

--- map/places/models.py
+++ map/places/models.py
@@ -2,26 +2,25 @@ from django.db import models
 from tinymce.models import HTMLField
 
 
-class Places(models.Model):
+class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.TextField()
     description_long = HTMLField()
-    lng = models.CharField(max_length=200)
-    lat = models.CharField(max_length=200)
-    place_id = models.CharField(max_length=25)
+    lng = models.FloatField(max_length=200)
+    lat = models.FloatField(max_length=200)
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='image', null=True)
-    image_id = models.IntegerField(default=1, db_index=True, unique=True)
-    places = models.ForeignKey(Places, on_delete=models.CASCADE, related_name='img')
+    image = models.ImageField(upload_to='image')
+    image_number = models.IntegerField()
+    places = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='imgs')
 
     def __str__(self):
-        return f'{self.places} - {self.image_id}'
+        return f'{self.places} - {self.image_number}'
 
     class Meta:
-        ordering = ['image_id']
+        ordering = ['image_number']
 
