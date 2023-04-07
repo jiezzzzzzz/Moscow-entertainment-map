e8a2319792c651532d3c721de37a8e719408901e
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-04-07 15:21:53 +0300

    refactoring

--- map/places/urls.py
+++ map/places/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import start_page, place_details
+from .views import start_page, get_place_details
 
 
 urlpatterns = [
     path('', start_page),
-    path('places/<int:place_id>/', place_details, name='places')
+    path('places/<int:place_id>/', get_place_details, name='places')
 ]
